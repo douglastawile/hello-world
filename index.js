a46aab7066ce4bdb2317471fbc5c727244e901aa
Douglas Maalongaye <douglastawile@gmail.com>
2023-04-25 10:51:18 +0000

    Added image to Hello World

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const paraEl = document.getElementById("demo");
+const paraEl = document.querySelectorAll("demo");
 const btnEl = document.getElementById("btn");
 
 btnEl.addEventListener("click", () => {
   paraEl.textContent = "🎉 Am a git guru for my projects.";
+  let name = prompt("Please enter your name?")
+  alert(`🎉🎉🎉! Thank you ${name} for coming here. Please this site is for demonstrative purpose and the button is inactive for now.`);
+  paraEl.textContent = `You will see new post ${name} every two weeks. Thank you 👋👋👋`;
 });
